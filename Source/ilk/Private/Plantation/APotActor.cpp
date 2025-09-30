235aa71a9fffb73bdc96f11c7971ba561145638b
egesmajha <162180604+egesmajha@users.noreply.github.com>
2025-09-30 21:13:47 +0300

    fixes

---
+++ Source/ilk/Private/Plantation/APotActor.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Plantation/APotActor.h"
+
+// Sets default values
+AAPotActor::AAPotActor()
+{
+ 
+    Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+    SetRootComponent(Root);
+    PotMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PotMesh"));
+    PotMesh->SetupAttachment(Root);
+
+}
+
+// Called when the game starts or when spawned
+void AAPotActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
+void AAPotActor::PlantSeed(TSubclassOf<AAStrainActor> PlantClass)
+{
+
+    if (CurrentPlant) return;
+
+    if (StrainClass) {
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.Owner = this;
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::/*AdjustIfPossibleBut*/AlwaysSpawn;
+
+        FVector Location = GetActorLocation() + FVector(0.f, 0.f, 50.f);
+        FRotator Rotation = FRotator::ZeroRotator;
+
+        CurrentPlant = GetWorld()->SpawnActor<AAStrainActor>(StrainClass, Location, Rotation, SpawnParams);
+    }
+
+    if (CurrentPlant) {
+        CurrentPlant->AttachToActor(this, FAttachmentTransformRules::KeepWorldTransform);
+        CurrentPlant->StartGrowth(this);
+    }
+}
+
---
+++ Source/ilk/Public/Plantation/APotActor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GAS/AStrainActor.h"
+#include "APotActor.generated.h"
+
+UCLASS()
+class ILK_API AAPotActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAPotActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+    USceneComponent* Root;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* PotMesh;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+    AAStrainActor* CurrentPlant;
+
+    UFUNCTION(BlueprintCallable, Category = "Pot")
+    void PlantSeed(TSubclassOf<AAStrainActor> StrainClass);
+};
